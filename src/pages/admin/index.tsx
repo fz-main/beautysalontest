963b0b7fcd2e7abf47f8f3e58db7e7385c3673cd
fz-main <velowebfz@proton.me>
2026-06-24 17:17:15 +0300

    fix: экспорт AdminPage

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,8 +1,1 @@
-import React from 'react';
-import { AdminLogin } from './AdminLogin';
-import { AdminDashboard } from './AdminDashboard';
-
-export const AdminPage: React.FC = () => {
-  const isLoggedIn = localStorage.getItem('admin_logged_in') === 'true';
-  return isLoggedIn ? <AdminDashboard /> : <AdminLogin />;
-};
+export { default as AdminPage } from './AdminDashboard';
